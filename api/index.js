99b8a33b5d92314cc15ec603a08b7e0c719b1983
CLCK0622 <zhongyi070622@163.com>
2024-08-10 20:18:57 +0800

    change vercel config

--- api/index.js
+++ api/index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/css/styles.css'));
+  });
+
 app.post('/upload', upload.single('image'), async (req, res) => {
     try {
         const timestamp = Date.now();
